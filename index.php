dc1ff9166dc8f04fa13e72c0b361bc2c32065197
introvertxboy-cpu <introvertxboy@gmail.com>
2025-11-28 18:47:49 +0500

    Update JSON response in index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-Type: application/json');
 echo json_encode([
-    'status' => 'online',
-    'service' => 'GoldLab Auth Server',
-    'endpoint' => 'POST /login.php'
+    'status' => 'online', 
+    'service' => 'GoldLab Auth',
+    'timestamp' => time()
 ]);
 ?>
